8feaf7fc6ab9276a71ebc7115d76a4c04d5afcab
Daniel Mateu <danielmateu86@gmail.com>
2022-09-22 15:15:56 +0200

    Formulario y useState

--- components/ui/NewEntry.tsx
+++ components/ui/NewEntry.tsx
@@ -1,47 +1,84 @@
+import React, { ChangeEvent, useState } from 'react'
+
 import { Box, Button, TextField } from '@mui/material'
-import React from 'react'
 import BookmarkAddedOutlinedIcon from '@mui/icons-material/BookmarkAddedOutlined';
 import BookmarkRemoveOutlinedIcon from '@mui/icons-material/BookmarkRemoveOutlined';
 import AddIcon from '@mui/icons-material/AddCommentOutlined';
 
+
 export const NewEntry = () => {
+
+    const [isAdding, setIsAdding] = useState(false);
+    const [inputValue, setInputValue] = useState('');
+    const [touched, setTouched] = useState(false);
+
+    const onTextFieldChanged = (e: ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value)
+    }
+
+    const onSave = () => {
+        if(inputValue.length === 0) return;
+        console.log(inputValue)
+
+    }
+
     return (
-        <Box sx = {{marginBottom: 2, paddingX: 1}}>
-        
+        <Box sx={{ marginBottom: 2, paddingX: 1 }}>
 
-        <Button 
-        startIcon = {<AddIcon/>}
-        fullWidth
-        variant='outlined'
-        
-        >
-            Agregar tarea
-        </Button>
+            {
+                isAdding ?
+                    (<>
+                        <TextField
+                            fullWidth
+                            sx={{ marginTop: 2, marginBottom: 1 }}
+                            placeholder='Nueva entrada'
+                            multiline
+                            label='Nueva entrada'
+                            helperText={inputValue.length <= 0 && touched && 'Ingrese un texto'}
+                            error = {inputValue.length <= 0 && touched}
+                            value={inputValue}
+                            onChange = {onTextFieldChanged}
+                            onBlur = {() => setTouched(true)}
+                        />
 
-        <TextField 
-        fullWidth
-        sx = {{marginTop:2, marginBottom: 1}}
-        placeholder='Nueva entrada'
-        multiline
-        label='Nueva entrada'
-        helperText='Ingrese un texto'
-        />
+                        <Box display="flex" justifyContent='space-between' paddingBottom={1}>
+                            <Button
+                                variant='outlined'
+                                color='primary'
+                                endIcon={<BookmarkAddedOutlinedIcon />}
+                                onClick={onSave}
+                            >
+                                Guardar
+                            </Button>
+                            <Button
+                                variant='outlined'
+                                color='warning'
+                                endIcon={<BookmarkRemoveOutlinedIcon />}
+                                onClick={() => setIsAdding(false)}
+                            >
+                                cancelar
+                            </Button>
 
-        <Box display="flex" justifyContent='space-between' paddingBottom={1}>
-            <Button
-                variant='outlined'
-                color='primary'
-                endIcon={<BookmarkAddedOutlinedIcon />}>
-                Guardar
-            </Button>
-            <Button
-                variant='outlined'
-                color='secondary'
-                endIcon={<BookmarkRemoveOutlinedIcon />}>
-                cancelar
-            </Button>
+                        </Box>
+                    </>)
+                    :
+                    (
+                        <Button
+                            startIcon={<AddIcon />}
+                            fullWidth
+                            variant='outlined'
+                            onClick={() => setIsAdding(true)}
+                        >
+                            Añadir tarea
+                        </Button>)
+            }
 
-        </Box>
+
+
+
+
+
+
         </Box>
     )
 }
