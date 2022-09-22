8db40eb659c03e9ad386331010f85f4591b0e49e
Daniel Mateu <danielmateu86@gmail.com>
2022-09-22 11:52:52 +0200

    Diseño de la pantalla principal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,35 @@
-import { Typography } from '@mui/material';
 import type { NextPage } from 'next'
+
+import { Typography, Grid, Card, CardHeader, CardContent } from '@mui/material';
 import { Layout } from '../components/layouts';
 
 
 const HomePage: NextPage = () => {
   return (
-    <Layout title="Hola mundo" >
-      <Typography variant='h1' color='primary'>Hola mundo! 😉</Typography>
+    <Layout title="Home - OpenJira" >
+      {/* <Typography variant='h1' color='primary'>Hola mundo! 😉</Typography> */}
+      <Grid container spacing={2}>
+
+        <Grid item xs={12} sm={4} xl={1}>
+          <Card sx = {{height: 'calc(100vh - 100px)'}}>
+            <CardHeader title='Pendientes' />
+            <CardContent>
+              {/* Agregar nueva tarea */}
+              {/* Listado de las entradas */}
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4} xl={1}>
+          <Card sx = {{height: 'calc(100vh - 100px)'}} >
+            <CardHeader title='En Progreso' />
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4} xl={1}>
+          <Card sx = {{height: 'calc(100vh - 100px)'}}>
+            <CardHeader title='Completadas' />
+          </Card>
+        </Grid>
+      </Grid>
     </Layout>
   )
 }
