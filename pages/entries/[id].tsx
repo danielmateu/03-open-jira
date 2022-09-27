6b9a226c5a170fb346059c50602353b7e2cc1718
Daniel Mateu <danielmateu86@gmail.com>
2022-09-27 12:52:32 +0200

    Manejo del formulario

--- pages/entries/[id].tsx
+++ pages/entries/[id].tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react'
+
 import { Layout } from '../../components/layouts';
 import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined';
-import { Grid, Card, CardHeader, CardContent, TextField, CardActions, Button } from '@mui/material';
+import BookmarkRemoveOutlinedIcon from '@mui/icons-material/BookmarkRemoveOutlined';
+import { Grid, Card, CardHeader, CardContent, TextField, CardActions, Button, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, capitalize, IconButton } from '@mui/material';
+import { EntryStatus } from '../../interfaces';
+// import { EntryStatus } from '../../interfaces/entry';
+
+
+const validStatus = ['pending', 'in-progress', 'finished']
 
 export const EntryPage = () => {
+
+    const [inputValue, setInputValue] = useState('');
+    const [status, setStatus] = useState<EntryStatus>('pending');
+    const [touched, setTouched] = useState(false);
+
+    const onInputValueChanged = (e: ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value)
+    }
+
+    const onStatusChanged =  (e: ChangeEvent<HTMLInputElement>) => {
+        setStatus(e.target.value as EntryStatus);
+    }
+
+    const onSave = () => {
+        console.log({inputValue, status});
+    }
+
     return (
         <Layout title=".....">
             <Grid
@@ -14,7 +38,7 @@ export const EntryPage = () => {
             >
                 <Grid item xs={12} sm={8} md={6}>
                     <Card>
-                        <CardHeader title="Entrada:"
+                        <CardHeader title={`Entrada: ${inputValue}`}
                             subheader={`Creada hace ... minutos`}
                         />
 
@@ -26,9 +50,34 @@ export const EntryPage = () => {
                                 autoFocus
                                 multiline
                                 label='Nueva entrada'
+                                value = {inputValue}
+                                onChange={onInputValueChanged}
                             />
 
                             {/* RADIO */}
+                            <FormControl>
+                                <FormLabel>
+                                    Estado: 
+                                </FormLabel>
+                                <RadioGroup
+                                    row
+                                    value= {status}
+                                    onChange={onStatusChanged}
+                                    >
+                                    {
+                                        validStatus.map(option => (
+                                            <FormControlLabel
+                                                key = {option}
+                                                value={option}
+                                                control={<Radio/>}
+                                                label={capitalize(option)}
+                                                
+                                            />
+                                        ))
+                                    }
+                                </RadioGroup>
+                            </FormControl>
+
                         </CardContent>
 
                         <CardActions>
@@ -36,6 +85,7 @@ export const EntryPage = () => {
                                 startIcon={<BookmarkBorderOutlinedIcon />}
                                 variant='contained'
                                 fullWidth
+                                onClick={onSave}
                             >
 
                                 Save
@@ -48,6 +98,15 @@ export const EntryPage = () => {
                 </Grid>
 
             </Grid>
+
+            <IconButton sx={{ 
+                position: 'fixed',
+                bottom: 30,
+                right: 30,
+                backgroundColor: 'error.dark'
+            }}>
+                <BookmarkRemoveOutlinedIcon/>
+            </IconButton>
 
 
         </Layout>
